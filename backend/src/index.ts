b8afeb8a88925d8d520fd3df6da88bd80bc90cfb
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-29 07:48:38 +0100

    import newly created resolvers into server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,13 +4,16 @@ import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { initializeDataSource } from "./config/dbConfig";
 import { ComplementResolver } from "./resolvers/ComplementResolver";
+import { PlaceResolver } from "./resolvers/PlaceResolver";
+import { SubjectResolver } from "./resolvers/SubjectResolver";
+import { VerbResolver } from "./resolvers/VerbResolver";
 
 const PORT = 4000;
 
 const startServer = async () => {
   await initializeDataSource();
   const schema = await buildSchema({
-    resolvers: [ComplementResolver],
+    resolvers: [ComplementResolver, PlaceResolver, SubjectResolver, VerbResolver],
   });
   const server = new ApolloServer({ schema });
   const { url } = await startStandaloneServer(server, {
