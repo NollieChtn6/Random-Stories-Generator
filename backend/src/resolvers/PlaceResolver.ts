6eacad43e97065a35c211a7f30209db957229adc
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-11-29 07:45:50 +0100

    create PlaceResolver

---
+++ backend/src/resolvers/PlaceResolver.ts
@@ -0,0 +1,14 @@
+import { Query, Resolver } from "type-graphql";
+import { PlaceEntity } from "../entities/Places";
+
+@Resolver()
+export class VerbResolver {
+  @Query(() => [PlaceEntity])
+  async complements(): Promise<PlaceEntity[]> {
+    const places = await PlaceEntity.find();
+    if (!places) {
+      throw new Error("No places found");
+    }
+    return places;
+  }
+}
