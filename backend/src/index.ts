638271bb3e6a968a16f75adc4a0b9016aa64dec9
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-01 16:29:54 +0100

    import StoryResolver into schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,20 +7,27 @@ import { ComplementResolver } from "./resolvers/ComplementResolver";
 import { PlaceResolver } from "./resolvers/PlaceResolver";
 import { SubjectResolver } from "./resolvers/SubjectResolver";
 import { VerbResolver } from "./resolvers/VerbResolver";
+import { StoryResolver } from "./resolvers/StoryResolver";
 
 const PORT = 4000;
 
 const startServer = async () => {
-  await initializeDataSource();
-  const schema = await buildSchema({
-    resolvers: [ComplementResolver, PlaceResolver, SubjectResolver, VerbResolver],
-  });
-  const server = new ApolloServer({ schema });
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: PORT },
-  });
+	await initializeDataSource();
+	const schema = await buildSchema({
+		resolvers: [
+			ComplementResolver,
+			PlaceResolver,
+			SubjectResolver,
+			VerbResolver,
+			StoryResolver,
+		],
+	});
+	const server = new ApolloServer({ schema });
+	const { url } = await startStandaloneServer(server, {
+		listen: { port: PORT },
+	});
 
-  console.log(`Server ready at ${url}`);
+	console.log(`Server ready at ${url}`);
 };
 
 startServer();
