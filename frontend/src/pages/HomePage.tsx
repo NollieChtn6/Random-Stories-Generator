e3fd597f313492c182c149f8d8a8d526dcbdb86f
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-01 21:57:15 +0100

    import Loader component and add update page structure

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useLazyQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
 import { Button } from "../components/Button";
+import { Loader } from "../components/Loader";
 import { RANDOM_STORY_QUERY } from "../graphql/queries";
 
 export function HomePage() {
@@ -12,7 +13,6 @@ export function HomePage() {
 	const [message, setMessage] = useState<string | null>(null);
 
 	const handleClickOnGenerate = async () => {
-		setMessage("Chargement de l'histoire en cours...");
 		try {
 			const response = await generateRandomStory();
 			if (error) {
@@ -28,27 +28,32 @@ export function HomePage() {
 	};
 
 	useEffect(() => {
-		if (loading) {
-			setMessage("Chargement de l'histoire en cours...");
-		} else if (error) {
+		if (error) {
 			setMessage("Erreur lors du chargement de l'histoire");
 		} else if (data?.randomStory) {
 			setMessage(null);
 		}
-	}, [data, loading, error]);
+	}, [data, error]);
 
 	return (
-		<div>
-			<p>
-				Générez une histoire aléatoire en cliquant sur le bouton ci-dessous.
-			</p>
-			<Button
-				type="button"
-				label={"Générer une histoire"}
-				onClick={handleClickOnGenerate}
-			/>
-			{message && <p>{message}</p>}
-			{data?.randomStory && <p>{data.randomStory}</p>}
+		<div className="homepage-container">
+			<div className="instructions-container">
+				<p className="instructions">
+					Générez une histoire aléatoire en cliquant sur le bouton ci-dessous.
+				</p>
+				<div className="button-container">
+					<Button
+						type="button"
+						label={"Générer une histoire"}
+						onClick={handleClickOnGenerate}
+					/>
+				</div>
+			</div>
+			<div className="story-container">
+				{loading && <Loader />}
+				{message && <p>{message}</p>}
+				{data?.randomStory && <p className="story">{data.randomStory}</p>}
+			</div>
 		</div>
 	);
 }
