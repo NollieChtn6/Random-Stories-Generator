ab40fd45ba45b94a2ddaff071e84e6de7713b2f3
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-02 22:18:08 +0100

    import TopicEntity

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -3,21 +3,28 @@ import { ComplementEntity } from "../entities/Complements";
 import { PlaceEntity } from "../entities/Places";
 import { SubjectEntity } from "../entities/Subjects";
 import { VerbEntity } from "../entities/Verbs";
+import { TopicEntity } from "../entities/Topics";
 
 const dataSource = new DataSource({
-  type: "sqlite",
-  database: "./random-stories-generator.sqlite",
-  entities: [ComplementEntity, PlaceEntity, SubjectEntity, VerbEntity],
-  synchronize: true,
+	type: "sqlite",
+	database: "./random-stories-generator.sqlite",
+	entities: [
+		ComplementEntity,
+		PlaceEntity,
+		SubjectEntity,
+		VerbEntity,
+		TopicEntity,
+	],
+	synchronize: true,
 });
 
 export const initializeDataSource = () => {
-  dataSource
-    .initialize()
-    .then(() => {
-      console.log("Data Source has been initialized successfully");
-    })
-    .catch((error) => {
-      console.log("Error during Data Source initialization", error);
-    });
+	dataSource
+		.initialize()
+		.then(() => {
+			console.log("Data Source has been initialized successfully");
+		})
+		.catch((error) => {
+			console.log("Error during Data Source initialization", error);
+		});
 };
