7efe63b16bac11c40ee78fd1a7bfbbc5c21a07ab
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-02 22:17:45 +0100

    add foreign key to TopicEntity

--- backend/src/entities/Subjects.ts
+++ backend/src/entities/Subjects.ts
@@ -1,18 +1,31 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+	BaseEntity,
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	JoinColumn,
+	ManyToOne,
+} from "typeorm";
 import { ObjectType, Field } from "type-graphql";
+import { TopicEntity } from "./Topics";
 
 @Entity({ name: "subjects" })
 @ObjectType()
 export class SubjectEntity extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  @Field()
-  id!: number;
+	@PrimaryGeneratedColumn()
+	@Field()
+	id!: number;
 
-  @Column({ nullable: false })
-  @Field()
-  subject!: string;
+	@Column({ nullable: false })
+	@Field()
+	subject!: string;
 
-  @Column({ nullable: false })
-  @Field()
-  isPlural!: boolean;
+	@Column({ nullable: false })
+	@Field()
+	isPlural!: boolean;
+
+	@ManyToOne(() => TopicEntity, { nullable: true, onDelete: "SET NULL" })
+	@JoinColumn({ name: "topicId" })
+	@Field(() => TopicEntity, { nullable: true })
+	topic?: TopicEntity;
 }
