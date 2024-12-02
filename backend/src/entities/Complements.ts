cd06bbca8ba89081310faceb0e604c6d101e1706
Noëllie CHATAIN SIX <noellie.chatain@hotmail.com>
2024-12-02 22:17:26 +0100

    add foreign key to TopicEntity

--- backend/src/entities/Complements.ts
+++ backend/src/entities/Complements.ts
@@ -1,14 +1,27 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+	BaseEntity,
+	Entity,
+	PrimaryGeneratedColumn,
+	Column,
+	JoinColumn,
+	ManyToOne,
+} from "typeorm";
 import { ObjectType, Field } from "type-graphql";
+import { TopicEntity } from "./Topics";
 
 @Entity({ name: "complements" })
 @ObjectType()
 export class ComplementEntity extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  @Field()
-  id!: number;
+	@PrimaryGeneratedColumn()
+	@Field()
+	id!: number;
 
-  @Column({ nullable: false })
-  @Field()
-  complement!: string;
+	@Column({ nullable: false })
+	@Field()
+	complement!: string;
+
+	@ManyToOne(() => TopicEntity, { nullable: true, onDelete: "SET NULL" })
+	@JoinColumn({ name: "topicId" })
+	@Field(() => TopicEntity, { nullable: true })
+	topic?: TopicEntity;
 }
